7a7030a089c05c2437a3c3fefbcbade45bbc194c
Jordan Safford <jordansafford@gmail.com>
2019-10-25 16:11:04 -0700

    add JS functionality allowing nname to be displated after form is submitted

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
-    event.preventDefault();
     var name = $("input#name").val();
     var ans1 = parseInt($("input:radio[name=subject]:checked").val())
     var ans2 = parseInt($("input:radio[name=pets]:checked").val())
-    var ans3 = parseInt($("input:radio[name=subject]:checked").val())
+    var ans3 = parseInt($("input:radio[name=music]:checked").val())
     var ans4 = parseInt($("input:radio[name=season]:checked").val())
 
     var total = ans1 + ans2 + ans4
@@ -17,5 +16,10 @@ $(document).ready(function() {
       $("#resultThree").show();
     } else if (total >= 10 && total <= 12) {
       $("#resultFour").show();
+    }
+
+    $(".name").text(name);
+
+      event.preventDefault();
   });
 });

213a96e0abba61b855cb3e6d1e80af762a304c7a
Jordan Safford <jordansafford@gmail.com>
2019-10-25 16:51:04 -0700

    add read me

---
+++ README.md
@@ -0,0 +1,34 @@
+#Language Suggester
+
+#### This is a language survey made by me on October 25, 2019.
+
+### By Jordan Safford
+
+## Description
+
+A brief survey with four multiple choice questions that suggests a programming language for the user to learn.
+
+## Setup/Installation Requirements
+
+### Installation
+
+* Copy repo and clone to desktop.
+* Open file in preferred browser.
+
+
+## Known Bugs
+
+Answers aren't displaying after form is submitted.
+
+## Support and contact details
+
+Email: jordansafford@gmail.com
+
+## Technologies Used
+
+HTML 5, CSS ,Bootstrap, JavaScript/Jquery
+
+### License
+
+
+Copyright (c) 2019 Jordan Safford Creative Commons Licensing
--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,23 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
     var name = $("input#name").val();
-    var ans1 = parseInt($("input:radio[name=subject]:checked").val())
-    var ans2 = parseInt($("input:radio[name=pets]:checked").val())
-    var ans3 = parseInt($("input:radio[name=music]:checked").val())
-    var ans4 = parseInt($("input:radio[name=season]:checked").val())
+    var ans1 = parseInt($("input:radio[name=subject]:checked").val());
+    var ans2 = parseInt($("input:radio[name=pets]:checked").val());
+    var ans3 = parseInt($("input:radio[name=music]:checked").val());
+    var ans4 = parseInt($("input:radio[name=season]:checked").val());
 
-    var total = ans1 + ans2 + ans4
+    var total = ans1 + ans2 + ans3 + ans4
 
     if (total >= 4 && total <= 6) {
       $('#resultOne').show();
-    } else if (total >= 6 && total <= 8) {
-      $("#resultTwo").show();
-    } else if (total >= 8 && total <= 10) {
-      $("#resultThree").show();
-    } else if (total >= 10 && total <= 12) {
-      $("#resultFour").show();
     }
+    // } else if (total >= 6 && total <= 8) {
+    //   $("#resultTwo").show();
+    // } else if (total >= 8 && total <= 10) {
+    //   $("#resultThree").show();
+    // } else if (total >= 10 && total <= 12) {
+    //   $("#resultFour").show();
+    // }
 
     $(".name").text(name);
 

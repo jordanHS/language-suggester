ecf5e8621dc15f3934e7bd74981a38b2d916d905
jordanHS <53791340+jordanHS@users.noreply.github.com>
2019-10-27 09:24:00 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -11,14 +11,14 @@ $(document).ready(function() {
 
     if (total >= 4 && total <= 6) {
       $('#resultOne').show();
-    }
+    
     // } else if (total >= 6 && total <= 8) {
     //   $("#resultTwo").show();
     // } else if (total >= 8 && total <= 10) {
     //   $("#resultThree").show();
     // } else if (total >= 10 && total <= 12) {
     //   $("#resultFour").show();
-    // }
+    }
 
     $(".name").text(name);
 
